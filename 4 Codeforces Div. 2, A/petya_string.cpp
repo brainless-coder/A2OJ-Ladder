a9a70da8a270d75967690b15856749935ecf0a2b
Prime <singhamish27@gmail.com>
2020-09-23 18:16:32 +0530

    petya and strings

---
+++ 4 Codeforces Div. 2, A/petya_string.cpp
@@ -0,0 +1,23 @@
+// Question Link: https://codeforces.com/problemset/problem/112/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+
+    if (a < b) {
+        cout << "-1";
+    } else if (a > b) {
+        cout << "1";
+    } else if (a == b) {
+        cout << "0";
+    }
+
+
+    return 0;
+}
